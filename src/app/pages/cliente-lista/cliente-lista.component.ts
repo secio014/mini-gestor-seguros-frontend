444de8d060dc88f001e5e554f542667bce47a607
Pedro Schmitz Sécio <pedro.secio014@gmail.com>
2026-03-13 13:36:06 -0300

    Adds confirmation dialog for client deletion
    Introduces a confirmation dialog before deleting a client to prevent accidental removals and enhance user experience by requiring explicit user confirmation for destructive actions.

--- src/app/pages/cliente-lista/cliente-lista.component.ts
+++ src/app/pages/cliente-lista/cliente-lista.component.ts
@@ -3,17 +3,21 @@ import { TableModule } from 'primeng/table';
 import { ClienteService } from '../../services/cliente.service'; // Importamos o serviço
 import { Cliente } from '../../models/cliente';
 import { Button } from "primeng/button"; // Import Interface
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
 
 @Component({
   selector: 'app-cliente-lista',
   standalone: true,
-  imports: [TableModule, Button],
+  imports: [TableModule, Button, ConfirmDialogModule],
+  providers: [ConfirmationService],
   templateUrl: './cliente-lista.component.html',
   styleUrl: './cliente-lista.component.css'
 })
 
 export class ClienteListaComponent implements OnInit {
   private clienteService = inject(ClienteService);
+  private confirmarDialog = inject(ConfirmationService);
   clientes: Cliente[] = []; // Começa vazia agora
 
   ngOnInit(): void {
@@ -27,8 +31,15 @@ export class ClienteListaComponent implements OnInit {
   }
 
   excluirCliente (id: string) {
-    this.clienteService.excluir(id).subscribe(() => {
-        this.listar();
-    });
+    this.confirmarDialog.confirm({
+      icon: 'pi pi-exclamation-triangle',
+      header: 'Confirmação de exclusão',
+      message: 'Tem certeza que deseja excluir este cliente?',
+      accept:() => {
+        this.clienteService.excluir(id).subscribe(() => {
+            this.listar();
+        })
+      }
+    })
   }
 }
