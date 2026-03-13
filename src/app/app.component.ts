02c3f8ba885e7a37869c911689f65b7f6cb58e1b
Pedro Schmitz Sécio <pedro.secio014@gmail.com>
2026-03-13 07:15:19 -0300

    Adds navigation menu to main layout
    Introduces a menu bar for easier navigation between sections.
    Improves user experience by providing clear access to core pages.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
 import { ClienteListaComponent } from "./pages/cliente-lista/cliente-lista.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ClienteListaComponent],
+  imports: [RouterOutlet, MenubarModule, ClienteListaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'mini-gestor-seguros-frontend';
+
+  itensDoMenu: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.itensDoMenu = [
+      {
+        label: 'Início',
+        icon: 'pi pi-home'
+      },
+      {
+        label: 'Clientes',
+        icon: 'pi pi-users'
+      }
+    ]
+
+  }
 }
